d98f1d5bf3ca91b07dae4687d6e17193fb3fd90a
CarolHGray <fivegrays@yahoo.com>
2024-02-07 18:08:21 -0600

    changed wording from type to foodGroup in Model folder

--- models/Menu.js
+++ models/Menu.js
@@ -10,7 +10,7 @@ class Menu extends Model {}
         autoIncrement: true,
       },
 
-        type: {
+        foodGroup: {      
           type: DataTypes.STRING,
           allowNull: false,
           primaryKey: false,
