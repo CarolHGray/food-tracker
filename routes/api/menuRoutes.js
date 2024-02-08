331a7cc022a6a0e9cd6e1b925fc20497a46c9b2d
CarolHGray <fivegrays@yahoo.com>
2024-02-07 19:25:18 -0600

    Reconfigured menuRoutes.js file and reconfigured server.js

--- routes/api/menuRoutes.js
+++ routes/api/menuRoutes.js
@@ -2,6 +2,17 @@ const Menu = require('../../models/Menu');
 
 const router = require('express').Router();
 
+// route to get all dishes
+router.get('/', async (req, res) => {
+  const menuData = await Menu.findAll().catch((err) => { 
+      res.json(err);
+    });
+     const menuItems = menuData.map((menuItem) => menuItem.get({ plain: true }));
+     res.render('index', { menuItems });
+//res.json(menuData);
+
+    });
+
 router.post('/', async (req, res) => {
     
     Menu.create(req.body).then((menuItem)=>res.json(menuItem))
