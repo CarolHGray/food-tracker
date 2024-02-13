2772055dc1128b5f1ed67d92b57397b21674a4ef
artificialair <jaybaker964@gmail.com>
2024-02-12 19:30:44 -0500

    added logout functionality + fixed redirects

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,16 +1,32 @@
 const router = require('express').Router();
 const { User } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
-  res.render('index');
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['name', 'ASC']],
+    });
+
+    const users = userData.map((project) => project.get({ plain: true }));
+
+    res.render('dashboard', {
+      users,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/login', (req, res) => {
-  res.render('login');
-});
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
 
-router.get('/dashboard', (req, res) => {
-  res.render('dashboard');
+  res.render('login');
 });
 
 module.exports = router;
