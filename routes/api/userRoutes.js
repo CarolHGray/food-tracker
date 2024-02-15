6e15a9a11172ff1f3cd84b0f0dfca4a6097284af
artificialair <jaybaker964@gmail.com>
2024-02-14 21:17:25 -0500

    added sign-up option to login handlebar

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,45 +2,64 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 router.post('/login', async (req, res) => {
-    try {
-      const dbuserData = await User.findOne({ where: { email: req.body.email } });
-  
-      if (!dbuserData) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
-      }
-  
-      const validPassword = await dbuserData.checkPassword(req.body.password);
-  
-      if (!validPassword) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
-      }
-  
-      req.session.save(() => {
-        req.session.userId = dbuserData.id;
-        req.session.loggedIn = true;
-        
-        res.redirect('/');
-      });
-  
-    } catch (err) {
-      res.status(400).json(err);
+  try {
+    const dbUserData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
     }
-  });
+
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.userId = dbUserData.id;
+      req.session.loggedIn = true;
+      
+      res.redirect('/');
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
   
-  router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-      req.session.destroy(() => {
-        res.redirect('/login');
-      });
-    } else {
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
       res.redirect('/login');
-    }
-  });
+    });
+  } else {
+    res.redirect('/login');
+  }
+});
+
+router.post('/signup', async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      name: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    });
+
+    req.session.save(() => {
+      req.session.userId = dbUserData.id;
+      req.session.loggedIn = true;
+      
+      res.redirect('/');
+    });
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
   
-  module.exports = router;
+module.exports = router;
